e6f0d3f9a7ed6ec3b45c36d332cb8b70368484d7
Lars Noergaard Nielsen <larsnn@gmail.com>
2015-02-25 16:05:32 -0800

    Client now execute c++ exe and performs challenge. Output is fed back to c# program.

---
+++ MFAYubiCryptClient/MFAYubiCryptClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFAYubiCryptClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string filename = Path.Combine(path, "YkLibTest.exe");
+
+            Console.WriteLine(filename);
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = filename,
+                Arguments = "Challenge",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                //RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+
+            var proc = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            proc.Start();
+            //proc.StandardInput.Write("c");
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+
+            //proc.StandardInput.WriteLine("c");
+
+                string line = proc.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        
+        }
+    }
+}
--- MFAYubiCryptClient/YubikeyLib/YubikeyLibTest.cpp
+++ MFAYubiCryptClient/YubikeyLib/YubikeyLibTest.cpp
@@ -15,6 +15,7 @@
 #include <yklib.h>
 #include <ctype.h>
 #include <conio.h>
+#include  <string.h>
 
 // NIST test vectors for HMAC-SHA1
 
@@ -74,7 +75,7 @@ void printRetcode(YKLIB_RC rc)
     }
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, char *argv[])
 {
     CYkLib yk;
     BYTE ch;
@@ -89,7 +90,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
     memset(&status, 0, sizeof(status));
 
-    for (;;) {
+    //for (;;) {
+		/*
         printf("\n\n");
 
         printf("a - Enumerate ports with Yubikeys present\n");
@@ -111,213 +113,16 @@ int _tmain(int argc, _TCHAR* argv[])
         printf("q - Poll readStatus\n");
 
         printf("\nSelect a..o > ");
+		*/
 
-        ch = tolower(_getch());
+	for (i = 0; i < 5; i=i+2) printf("%c", argv[1][i]);
+		//printf("%s", argv[1]);
+
+	yk.openKey();
+	char *cha = "m";
+
        
-        switch (ch) {
 
-            case 0x1b:
-                return 1;
-
-                // Enumerate all ports with Yubikeys present and return count
-
-            case 'a':
-                printf("Number of Yubikeys found %d", yk.enumPorts());
-                break;
-
-                // Print out port names from previous enumeration
-
-            case 'b':
-                for (i = 0; ; i++) {
-                    if (!yk.getPortName(i, buf, sizeof(buf) / sizeof(buf[0]))) break;
-                    wprintf(L"\n%2d - %s", i, buf); 
-                }
-                break;
-
-                // Open one key. If none or more than one key is attached, the call will fail
-
-            case 'c':
-                printRetcode(yk.openKey());
-                break;
-
-                // Open a specific key based on index from a previous enumeration           
-
-            case 'd':
-                printRetcode(yk.openKey((unsigned short) 0));
-                break;
-
-                // Close an open Yubikey
-
-            case 'e':
-                printRetcode(yk.closeKey());
-                break;
-
-                // Read out the status block from an opened Yubikey
-
-            case 'f':
-                if ((rc = yk.readStatus(&status)) == YKLIB_OK) {
-                    printf("Version %d.%d.%d Touch %d", status.versionMajor, status.versionMinor, status.versionBuild, status.touchLevel);
-                    printf("\nSlot configuration status:\nSlot 0 - ");
-                    printRetcode(yk.isSlotConfigured(YKLIB_FIRST_SLOT, &status));
-                    printf(" Slot 1 - ");
-                    printRetcode(yk.isSlotConfigured(YKLIB_SECOND_SLOT, &status));
-                } else
-                    printRetcode(rc);
-                break;
-
-                // Write a configuration record, Yubico OTP or challenge-response
-
-            case 'g':
-            case 'h':
-                // Clear unused fields
-
-                memset(&config, 0, sizeof(config));
-
-                // Fill in desired settings in the config structure here
-
-                if (ch == 'g') {
-
-                    // Yubico OTP
-                    
-                    // 2 bytes public id 0x4711 == fibb modhex
-
-                    config.fixedSize = 2;
-                    config.fixed[0] = 0x47;
-                    config.fixed[1] = 0x11;
-
-                    // Set private ID here
-
-                    memcpy(config.uid, "\x1\x2\x3\x4\x5\x6", sizeof(config.uid));
-
-                    // Set AES-128 key here
-
-                    memset(config.key, 0x55, sizeof(config.key));
-
-                    // Set flags tktFlags, cfgFlags and extFlags to values from ykdef.h
-
-                    config.tktFlags = TKTFLAG_APPEND_CR;
-                    config.extFlags = EXTFLAG_SERIAL_API_VISIBLE | EXTFLAG_SERIAL_BTN_VISIBLE;
-                } else {
-
-                    // HMAC-SHA1 for Challenge-response, using a NIST key
-
-                    yk.setKey160(&config, nistKey);
-
-                    // Set flags tktFlags, cfgFlags and extFlags to values from ykdef.h
-
-                    config.tktFlags = TKTFLAG_CHAL_RESP;
-                    config.cfgFlags = CFGFLAG_CHAL_HMAC | CFGFLAG_HMAC_LT64 | CFGFLAG_CHAL_BTN_TRIG;
-                    config.extFlags = EXTFLAG_SERIAL_API_VISIBLE;
-                }
-
-                // Perform configuration write
-
-                timerStart();
-                rc = yk.writeConfigBegin(targetSlot, &config);
-                printRetcode(rc);
-                printf(" twrite=%lu ms ", timerQuery());
-
-                // If write was initiated successful, wait for completion
-
-                if (rc == YKLIB_OK) {
-                    rc = yk.waitForCompletion(YKLIB_MAX_WRITE_WAIT);
-                    printRetcode(rc);
-                    printf(" total=%lu ms", timerQuery());
-                }
-                break;
-
-                // Kill a configuration in an opened Yubikey
-
-            case 'i':
-
-                // Initialize write (no data = kill)
-
-                rc = yk.writeConfigBegin(targetSlot);
-
-                // If successfully initiated, wait for completion
-
-                if (rc == YKLIB_PROCESSING) rc = yk.waitForCompletion(YKLIB_MAX_WRITE_WAIT);
-                printRetcode(rc);
-                break;
-
-                // Read out the factory programmed serial number from an opened Yubikey
-
-            case 'j':
-
-                // Setup serial number read
-
-                timerStart(); 
-                rc = yk.readSerialBegin();
-                printf(" twrite=%lu ms ", timerQuery());
-                if (rc == YKLIB_OK) {
-
-                    // Wait for response completion
-
-                    rc = yk.waitForCompletion(YKLIB_MAX_SERIAL_WAIT, buffer, sizeof(DWORD));
-                    printf(" total=%lu ms ", timerQuery());
-
-                    if (rc == YKLIB_OK) {
-                        printf("\nSerial number read = %02x%02x%02x%02x", buffer[0], buffer[1], buffer[2], buffer[3]);
-                    } else
-                        printRetcode(rc);
-                } else
-                    printRetcode(rc);
-
-                break;
-
-                // Perform a challenge-response in Yubico OTP mode
-
-            case 'k':
-                memcpy(buffer, "\x1\x2\x3\x4\x5\x6", 6);
-                timerStart();
-                printRetcode(rc = yk.writeChallengeBegin(targetSlot, YKLIB_CHAL_OTP, buffer, UID_SIZE));
-                printf(" twrite=%lu ms ", timerQuery());
-
-                if (rc == YKLIB_OK) {
-
-                    // Wait for response completion
-
-                    rc = yk.waitForCompletion(YKLIB_MAX_CHAL_WAIT, buffer, sizeof(TICKET));
-                    printf(" total=%lu ms ", timerQuery());
-
-                    if (rc == YKLIB_OK) {
-                        printf("\nYubico OTP = ");
-                        for (i = 0; i < sizeof(TICKET); i++) printf("%02x", buffer[i]);
-                    } else
-                        printRetcode(rc);
-                } else
-                    printRetcode(rc);
-
-                break;
-
-                // Perform a challenge-response in HMAC-SHA1 mode
-
-            case 'l':
-                timerStart();
-                printRetcode(rc = yk.writeChallengeBegin(targetSlot, YKLIB_CHAL_HMAC, (BYTE *) nistChal, sizeof(nistChal)));
-                printf(" twrite=%lu ms ", timerQuery());
-
-                if (rc == YKLIB_OK) {
-
-                    // Wait for response completion
-
-                    rc = yk.waitForCompletion(YKLIB_MAX_CHAL_WAIT, buffer, SHA1_DIGEST_SIZE);
-                    printf(" total=%lu ms ", timerQuery());
-
-                    if (rc == YKLIB_OK) {
-                        printf("\nHMAC-SHA1 = ");
-                        for (i = 0; i < SHA1_DIGEST_SIZE; i++) printf("%02x", buffer[i]);
-                        if (!memcmp(buffer, nistResp, sizeof(nistResp))) printf(" = NIST response");
-                    } else
-                        printRetcode(rc);
-                } else
-                    printRetcode(rc);
-
-                break;
-
-                // Perform a challenge-response in HMAC-SHA1 mode and allow wait for button to be pressed
-
-            case 'm':
                 memcpy(buffer, "Sample #2", 9);
                 printRetcode(rc = yk.writeChallengeBegin(targetSlot, YKLIB_CHAL_HMAC, buffer, 9));
                 
@@ -352,54 +157,8 @@ int _tmain(int argc, _TCHAR* argv[])
                 } else
                     printRetcode(rc);
 
-                break;
-
-                // Write NDEF URI record
-
-            case 'n':
-
-                // Initialize write (no data = kill)
-
-                rc = yk.writeNDEFBegin(targetSlot, YKLIB_NDEF_URI, "http://demo.yubico.com/php-yubico/one_factor.php?key=");
-
-                // If successfully initiated, wait for completion
-
-                if (rc == YKLIB_PROCESSING) rc = yk.waitForCompletion(YKLIB_MAX_WRITE_WAIT);
-                printRetcode(rc);
-                break;
-
-                // Abort a pending request (if any)
-
-            case 'o':
-                printRetcode(yk.abortPendingRequest());
-                break;
-
-            case 'p':
-                if (targetSlot == YKLIB_FIRST_SLOT) {
-                    printf("Target is now YKLIB_SECOND_SLOT");
-                    targetSlot = YKLIB_SECOND_SLOT;
-                } else {
-                    printf("Target is now YKLIB_FIRST_SLOT");
-                    targetSlot = YKLIB_FIRST_SLOT;
-                }
-                break;
-
-            case 'q':
-                printf("\nPress any key to quit... ");
-                while (!_kbhit()) {
-                    rc = yk.readStatus(&status);
-                    if (rc != YKLIB_OK) {
-                        printf("\nTerminated with retcode ");
-                        printRetcode(rc);
-                        break;
-                    }
-                    printf("%d ", status.touchLevel);
-                    Sleep(250);
-                }
-                break;
-
-        }
-    }
+            
+    
 
 	return 0;
 }
